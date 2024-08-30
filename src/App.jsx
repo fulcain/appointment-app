3cdd78db7d5e463e414ab2612152977ad57bfcd5
Majid Kargar <majidkargar01@gmail.com>
2024-08-30 12:59:40 +0330

    Feat: Add fonts and basic App component
    I've added a font, linked it and add some basic styles to the App
    component.
    I've also added a css reset file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './css-reset.css';
+import './assest/font/fontiran.css';
+
+const App = () => {
+	return (
+		<div className='text-center mt-10'>
+			<h1 className='text-4xl text-white font-medium'>نوبت دهی آرایشگاه</h1>
+		</div>
+	);
+};
+
+export default App;
