bf652cb9c9f7399eda585fbd00bf42b2a353996d
Majid Kargar <majidkargar01@gmail.com>
2024-11-10 17:21:41 +0330

    Feat: Localize the MUI to persian

--- src/components/theme.ts
+++ src/components/theme.ts
@@ -1,19 +1,23 @@
 import { createTheme } from "@mui/material/styles";
+import { faIR } from "@mui/x-date-pickers/locales";
 
-const theme = createTheme({
-  direction: "rtl",
-  typography: {
-    fontFamily: "vazir, Roboto",
-  },
-  palette: {
-    mode: "dark",
-    primary: {
-      main: "#61AFEF",
+const theme = createTheme(
+  {
+    direction: "rtl",
+    typography: {
+      fontFamily: "vazir, Roboto",
     },
-    secondary: {
-      main: "#c678DD",
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#61AFEF",
+      },
+      secondary: {
+        main: "#c678DD",
+      },
     },
   },
-});
+  faIR,
+);
 
 export default theme;
