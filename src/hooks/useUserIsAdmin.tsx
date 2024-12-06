f0f91f85afcc34a7dcf112ffad32a02cb3cce4a4
Majid Kargar <majidkargar01@gmail.com>
2024-12-06 15:14:12 +0330

    Refactor: change the useUserIsAdmin hook

--- src/hooks/useUserIsAdmin.tsx
+++ src/hooks/useUserIsAdmin.tsx
@@ -1,9 +1,33 @@
 import { useEffect, useState } from "react";
-import { supabase } from "../utils/supabase";
+import { supabase } from "../utils/supabase/supabase";
 
 const useUserIsAdmin = () => {
+  const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
+    (async () => {
+      const { data: profiles, error } = await supabase
+        .from("profiles")
+        .select("*");
+
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      if (user?.identities) {
+        const userId: string = user?.identities[0].user_id;
+
+        const userInProfiles = profiles?.find((profile) => {
+          return profile.id === userId;
+        });
+
+        if (userInProfiles) {
+          const isUserAdmin = Boolean(userInProfiles.isAdmin);
+
+          setIsAdmin(isUserAdmin);
+        }
+      }
+    })();
   }, []);
 
   return isAdmin;
